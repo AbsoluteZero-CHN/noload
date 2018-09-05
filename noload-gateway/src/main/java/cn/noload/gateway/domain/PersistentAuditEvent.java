e070b64c8c5142b43c27a6e9a7a80ef474c4688f
AbsoluteZero-CHN <caohao1209@gmail.com>
2018-09-05 22:33:05 +0800

    修改数据库为base

--- noload-gateway/src/main/java/cn/noload/gateway/domain/PersistentAuditEvent.java
+++ noload-gateway/src/main/java/cn/noload/gateway/domain/PersistentAuditEvent.java
@@ -1,5 +1,7 @@
 package cn.noload.gateway.domain;
 
+import org.hibernate.annotations.GenericGenerator;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
@@ -19,9 +21,10 @@ public class PersistentAuditEvent implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "event_id")
-    private Long id;
+    @GeneratedValue(generator="uuid2")
+    @GenericGenerator(name="uuid2",strategy="uuid2")
+    @Column(name="event_id", unique=true, nullable=false, updatable=false, length = 36)
+    private String id;
 
     @NotNull
     @Column(nullable = false)
@@ -39,11 +42,11 @@ public class PersistentAuditEvent implements Serializable {
     @CollectionTable(name = "t_sys_persistent_audit_evt_data", joinColumns=@JoinColumn(name="event_id"))
     private Map<String, String> data = new HashMap<>();
 
-    public Long getId() {
+    public String getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(String id) {
         this.id = id;
     }
 
